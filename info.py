1b2d31fc196ce3d41d8e8ce4b4df8e85f7643ef2
Wasim Akhtar <wasimakhtar.me@gmail.com>
2026-03-18 05:14:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,7 +126,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mdiskshort.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'ce99efd97d05ce5f3698112d843b01a1f6e735d6')
 TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
