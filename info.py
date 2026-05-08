99f5da14a8409e3594a80b3924ed15668ecf0fd0
Wasim Akhtar <wasimakhtar.me@gmail.com>
2026-05-08 10:30:49 +0530

    Update Heroku URL in info.py

--- info.py
+++ info.py
@@ -172,7 +172,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://rickautofilter-655ec27dbd13.herokuapp.com/")
+URL = environ.get("URL", "https://vlcfilterbot-90835b1f7b7e.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
