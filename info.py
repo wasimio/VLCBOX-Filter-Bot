ede8e5378ae218655a8f03c89da44b0a55442225
Wasim Akhtar <wasimakhtar.me@gmail.com>
2026-08-29 15:07:42 +0530

    Change default REQUEST_TO_JOIN_MODE to False

--- info.py
+++ info.py
@@ -36,11 +36,11 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 
 # auth_channel means force subscribe channel.
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
-REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', True)) # Set True Or False
+REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', False)) # Set True Or False
 TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', False)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
 
 # This Is Force Subscribe Channel, also known as Auth Channel 
-auth_channel = environ.get('AUTH_CHANNEL', '-1002385908762 -1002824317499 -1004331634066') # give your force subscribe channel IDs here separated by space
+auth_channel = environ.get('AUTH_CHANNEL', '-1002385908762 -1002824317499') # give your force subscribe channel IDs here separated by space
 AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in auth_channel.split()] if auth_channel else []
 
 # This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
